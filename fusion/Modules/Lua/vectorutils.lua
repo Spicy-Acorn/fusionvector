8b10b4fcaffcd01c6e5df55a45e427d72879cfdf
Cedric Duriau <devops@cc.be>
2020-03-16 17:51:45 +0100

    add docs

--- fusion/Modules/Lua/vectorutils.lua
+++ fusion/Modules/Lua/vectorutils.lua
@@ -7,6 +7,17 @@ local json = self and require("dkjson") or nil
 vectorutils = {}
 
 function vectorutils.vector_from_array(columns, array)
+    --[[
+        Builds a vector from an array.
+
+        :param cols: Column dimension of the vector.
+        :type cols: int
+
+        :param array: Table representing an array to build vector from.
+        :type array: table
+
+        :rtype: table
+    ]]
     local vec = {}
 
     for i=1,columns do
@@ -18,6 +29,14 @@ function vectorutils.vector_from_array(columns, array)
 end
 
 function vectorutils.vector_to_array(vec)
+    --[[
+        Builds an array from a vector.
+
+        :param vec: Vector to build array from.
+        :type vec: table
+
+        :rtype: table
+    ]]
     local arr = {}
 
     for _, v in ipairs(vec) do
@@ -28,11 +47,27 @@ function vectorutils.vector_to_array(vec)
 end
 
 function vectorutils.columns(vec)
+    --[[
+        Returns the amount of columns of a vector.
+
+        :param vec: Vector to get column count from.
+        :type vec: table
+
+        :rtype: number
+    ]]
     local columns = table.getn(vec)
     return columns
 end
 
 function vectorutils.vector_to_string(vec)
+    --[[
+        Builds an interchangeable json string from a vector.
+
+        :param vec: Vector to build json string from.
+        :type vec: table
+
+        :rtype: string
+    ]]
     -- vector to custom table
     local vec_table = {}
 
